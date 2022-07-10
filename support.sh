96672d1faddb5886fac3db0badf2ac2c9234f16f
Damar Mustiko Aji <damarresin@gmail.com>
2022-07-10 16:47:31 +0700

    Create support.sh

---
+++ support.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# from https://chromium.woolyss.com/
+# and https://gist.github.com/addyosmani/5336747
+# and https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+apt-get update
+
+if [ -z $(which node) ]; then
+    curl -sL https://deb.nodesource.com/setup_8.x | bash -
+    apt-get install -y nodejs
+fi
+
+if [ -z $(which npm) ]; then
+    apt-get install -y npm
+fi
+
+apt-get install -y libappindicator1 fonts-liberation
+apt-get -y install dbus-x11 xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+apt-get -f -y install
+dpkg --configure -a
+dpkg -i google-chrome*.deb
